1bab7868192059a5b736390fc91fae3aea174258
Tuan Thanh <tuanthanh00200@gmail.com>
2023-12-15 16:47:18 +0700

    up

--- app/controllers/api/v1/interview_controller.rb
+++ app/controllers/api/v1/interview_controller.rb
@@ -1,6 +1,20 @@
 class Api::V1::InterviewController < ApplicationController
   def index
-    interviews = Interview.all
-    render_response("Hiển thị danh sách lịch phỏng vấn", data: interviews)
+    interviews = Interview.all.to_a
+
+    result = convert_interviews_to_camel_case(interviews)
+    render_response("Hiển thị danh sách lịch phỏng vấn", data: result, status: 200)
+  end
+
+  private
+
+  def convert_interviews_to_camel_case(interviews)
+    interviews.map do |interview|
+      convert_keys_to_camel_case(interview.attributes)
+    end
+  end
+
+  def convert_keys_to_camel_case(hash)
+    hash.transform_keys { |key| key.to_s.camelize(:lower).to_sym }
   end
 end
